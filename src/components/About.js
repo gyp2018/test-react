c44d561d04c7e668384fce6db3a6651f3286116f
GimYoungPhil <youngphil.gim@gmail.com>
2018-01-17 15:45:24 +0900

    add about.css

--- src/components/About.js
+++ src/components/About.js
@@ -1,9 +1,10 @@
 import React from "react";
+import "./about.css";
 
 class About extends React.Component {
   render() {
     return (
-      <div>
+      <div className="about">
         <h2>About</h2>
       </div>
     );
