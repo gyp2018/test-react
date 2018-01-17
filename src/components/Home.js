db9979e4e1aeb51939bfe34801b1d21bbdaddf38
GimYoungPhil <youngphil.gim@gmail.com>
2018-01-17 15:37:01 +0900

    fixed Home.js

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,12 +1,9 @@
 import React from "react";
-
 import Jumbotron from "./home/Jumbotron";
-
 const Home = () => (
   <div>
     <h2>Home</h2>
     <Jumbotron name="hi" />
   </div>
 );
-
 export default Home;
